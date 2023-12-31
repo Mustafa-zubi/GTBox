efc7f77c85c378657078b43bc29b0656ff13fc01
None <None>
2023-12-31 02:44:46 -0800

    initial version

---
+++ envSetup.py
@@ -0,0 +1,33 @@
+# This class is used to build the network topology: 
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import Host, OVSSwitch
+from mininet.link import TCLink
+from mininet.cli import CLI
+
+class netTopology(Topo):
+    def __init__(self, hosts=2, switches=1, gw=1):
+        # initialization function that is executed whenever you call the class netTopology
+        # by default, if you don't assign number of hosts, switchws and gateway default values 
+        # will be added automatically -> hosts=2, switches=1, gw=1
+
+        # initialization 
+        self.hosts = hosts
+        self.switches = switches
+        self.gw = gw
+        print ("Hello")
+
+    def build(self):
+        # Add 5 switches and 5 hosts
+        switches = [self.addSwitch(f's{i}', cls=OVSSwitch) for i in range(1, 6)]
+        hosts = [self.addHost(f'h{i}') for i in range(1, 6)]
+
+        # Add links between switches and hosts
+        for i in range(5):
+            self.addLink(switches[i], hosts[i], cls=TCLink)
+
+        # Connect switches in a linear topology
+        for i in range(4):
+            self.addLink(switches[i], switches[i+1])
+
---
+++ main.py
@@ -0,0 +1,25 @@
+# This is the main program the runs GTBox. 
+# created by MZ on Dec 31, 2023.
+# Version control: V0.01
+
+from envSetup import netTopology 
+
+# def main():
+#     topo = CustomTopology()
+#     net = Mininet(topo=topo, controller=None)  # Use the default controller
+
+#     # Set IP addresses and default gateway for hosts
+#     for i in range(1, 6):
+#         host = net.get(f'h{i}')
+#         host.setIP(f'10.0.0.{i}/24')
+#         host.setDefaultRoute('via 10.0.0.6')  # Assuming the gateway is h6
+
+#     net.start()
+
+#     # Start the Mininet CLI
+#     CLI(net)
+
+#     net.stop()
+
+# if __name__ == '__main__':
+#     main()
